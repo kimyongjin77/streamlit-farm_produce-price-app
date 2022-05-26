c883e8f0243ed184744c059e80e00882224a471b
kimyongjin77 <yjkim770518@gmail.com>
2022-05-26 13:16:30 +0900

    수정

--- app_eda.py
+++ app_eda.py
@@ -22,7 +22,9 @@ elif platform.system() == 'Windows':
     rc('font', family=font_name)
 elif platform.system() == 'Linux':
     #matplotlib의 폰트를 Nanum 폰트로 지정합니다.
-    rc('font', family='NanumGothic')
+    path = " /usr/share/fonts/nanum/NanumGothic.ttf"
+    font_name = font_manager.FontProperties(fname=path).get_name()
+    rc('font', family=font_name)
 else:
     print('This System=' + platform.system() + ' Unknown system... sorry~~~~')
 
